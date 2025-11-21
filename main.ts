8e36d507ef8728ba85e3acbe8da00886b0e3ef50
noah-quetua <noah.quetua@stu.ocsb.ca>
2025-11-21 00:15:13 +0000

    The area of a circle

---
+++ main.ts
@@ -0,0 +1,21 @@
+/**
+ * @author Noah Quetua
+ * @version 1.0.0
+ * @date 2025-11-20
+ * @fileoverview This program is for the Area of a circle.
+ */
+
+// Value of pi
+console.log("Pi = 3.14")
+
+// Given radius 
+console.log("Radius = 5.6 cm")
+
+// Calculate the area
+console.log("A = Pi * r * r");
+console.log("3.14 * 5.6 * 5.6 = " + (3.14 * 5.6 * 5.6));
+
+// Display Sentence
+console.log("The area of the circle is 98.47cm squared")
+
+console.log("\nDone")
